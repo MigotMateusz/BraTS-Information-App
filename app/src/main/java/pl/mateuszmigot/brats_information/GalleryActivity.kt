24cb9c5e43ef77513d8c9eeac842ab45444f6ec4
Mateusz Migot <migotmateusz@gmail.com>
2021-11-19 22:02:31 +0100

    added base classes for activities

---
+++ app/src/main/java/pl/mateuszmigot/brats_information/GalleryActivity.kt
@@ -0,0 +1,11 @@
+package pl.mateuszmigot.brats_information
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery)
+    }
+}
---
+++ app/src/main/java/pl/mateuszmigot/brats_information/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package pl.mateuszmigot.brats_information
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
