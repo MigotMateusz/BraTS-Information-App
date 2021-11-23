578a3d0e8700b9fae252c4124825440f4915b9ef
Mateusz Migot <migotmateusz@gmail.com>
2021-11-23 19:31:33 +0100

    added base html page with web view for general info fragment and added dark mode capabilities for this web view

--- app/src/main/java/pl/mateuszmigot/brats_information/GeneralInfoFragment.kt
+++ app/src/main/java/pl/mateuszmigot/brats_information/GeneralInfoFragment.kt
@@ -1,16 +1,21 @@
 package pl.mateuszmigot.brats_information
 
+import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
+import androidx.webkit.WebSettingsCompat.*
+import androidx.webkit.WebViewFeature
 import pl.mateuszmigot.brats_information.databinding.FragmentGeneralInfoBinding
 
 class GeneralInfoFragment : Fragment() {
 
     private var _binding: FragmentGeneralInfoBinding? = null
+    private lateinit var webView: WebView
 
     private val binding get() = _binding!!
 
@@ -20,8 +25,10 @@ class GeneralInfoFragment : Fragment() {
     ): View? {
 
         _binding = FragmentGeneralInfoBinding.inflate(inflater, container, false)
+        webView = binding.root.findViewById<WebView>(R.id.WebView)
+        setDarkModeWithStrategyForWebView()
+        webView.loadUrl("file:///android_asset/index.html")
         return binding.root
-
     }
 
     override fun onDestroyView() {
@@ -33,5 +40,33 @@ class GeneralInfoFragment : Fragment() {
         super.onConfigurationChanged(newConfig)
         resources.configuration.setLocale(newConfig.locales.get(0))
         resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+    }
+
+    private fun setDarkModeWithStrategyForWebView() {
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+            setDarkModeStrategyIfPossible()
+            setDarkModeForWebView()
+        }
+    }
+
+    private fun setDarkModeStrategyIfPossible() {
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK_STRATEGY)) {
+            setForceDarkStrategy(
+                webView.settings,
+                DARK_STRATEGY_PREFER_WEB_THEME_OVER_USER_AGENT_DARKENING
+            )
+        }
+    }
+
+    @SuppressLint("RequiresFeature")
+    private fun setDarkModeForWebView() {
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                setForceDark(webView.settings, FORCE_DARK_ON)
+            }
+            Configuration.UI_MODE_NIGHT_NO, Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                setForceDark(webView.settings, FORCE_DARK_OFF)
+            }
+        }
     }
 }
